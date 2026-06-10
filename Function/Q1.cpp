d9b88a4c74a771fc146c53d7e19e284cbb6e13cc
Pallav Bhattarai <pallavbhattarai1995@gmail.com>
2026-06-10 19:58:39 +0545

    Update Q1.cpp
    update q1

--- Function/Q1.cpp
+++ Function/Q1.cpp
@@ -1,1 +1,34 @@
 // What are inline functions? Write C++ program demonstrating function overloading and default arguments.
+
+#include <iostream>
+using namespace std;
+
+// Function Overloading
+int add(int a, int b)
+{
+    return a + b;
+}
+
+float add(float a, float b)
+{
+    return a + b;
+}
+
+// Function with Default Arguments
+int multiply(int a, int b = 1)
+{
+    return a * b;
+}
+
+int main()
+{
+    // Function Overloading
+    cout << "Addition of two integers: " << add(10, 20) << endl;
+    cout << "Addition of two floating-point numbers: " << add(10.5f, 20.5f) << endl;
+
+    // Default Arguments
+    cout << "Multiplication with one argument: " << multiply(5) << endl;
+    cout << "Multiplication with two arguments: " << multiply(5, 4) << endl;
+
+    return 0;
+}
